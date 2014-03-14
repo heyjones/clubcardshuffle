fdccc8cbc08beb9f35f2c5c801e75e9ce02fc468
Chris Jones <cjones@dpsinc.com>
2014-03-14 11:14:43 -0700

    ...

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,7 +2,12 @@ class Club < ActiveRecord::Base
 
 	has_many :cards
 
-	has_attached_file :logo, :styles => { :thumb => '360x360>' }, :default_url => '/images/:style/default.png'
+	has_attached_file :logo,
+		:styles => { :thumb => '360x360>' },
+ 		:convert_options => {:original => '-quality 100 -strip', :thumb => '-quality 50 -strip' },
+		:path => ':rails_root/public/system/:class/:attachment/:id.:style.:extension',
+		:url => '/system/:class/:attachment/:id.:style.:extension',
+		:default_url => '/system/:class/:attachment/0.:style.:extension'
 	validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
 
 	validates_presence_of :name, :regex
